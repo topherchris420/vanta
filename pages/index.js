1237d5dd74fd871b86066905f85938cf7e9b097c
chris <93827718+topherchris420@users.noreply.github.com>
2023-04-06 23:03:30 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -39,3 +39,39 @@ export default function Home() {
 </a></h1><iframe src="https://giphy.com/embed/jnWMCLBfJb7CK4D8iY" width="340" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/pixelart-rain-programming-jnWMCLBfJb7CK4D8iY"></a></p> <a href="https://givebutter.com/h0CJIU">ᵈᵒⁿᵃᵗᵉ ᵃ ᶜᵒᶠᶠᵉᵉ</a> ₿ </div>
   );
 }
+// Define some responses
+const responses = {
+  greeting: "Hello! How are you doing today?",
+  good: "That's great to hear!",
+  bad: "I'm sorry to hear that. Is there anything I can do to help?",
+  bye: "Goodbye!"
+};
+
+// Get the chatbox elements
+const chatbox = document.getElementById("chatbox");
+const output = document.getElementById("output");
+const input = document.getElementById("input");
+
+// Start the chat
+output.innerHTML = responses.greeting;
+
+// Handle input
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) { // Enter key pressed
+    const message = input.value;
+    input.value = "";
+
+    // Check for keywords
+    if (message.toLowerCase().includes("good")) {
+      output.innerHTML += "<br>" + responses.good;
+    } else if (message.toLowerCase().includes("bad")) {
+      output.innerHTML += "<br>" + responses.bad;
+    } else if (message.toLowerCase().includes("bye")) {
+      output.innerHTML += "<br>" + responses.bye;
+    } else {
+      output.innerHTML += "<br>" + "I'm sorry, I didn't understand that.";
+    }
+  }
+});
+
+
