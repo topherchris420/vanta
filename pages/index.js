8dc9e7679113b9ab5ebfbde11bf308a79470f38f
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2024-04-07 10:21:40 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,12 @@ import * as THREE from "three";
 export default function Home() {
   const [vantaEffect, setVantaEffect] = useState(0);
   const vantaRef = useRef(null);
-
   useEffect(() => {
     if (!vantaEffect) {
       setVantaEffect(
         NET({
           el: vantaRef.current,
-          THREE: THREE,
+          THREE,
           color: 0x387C44,
           backgroundColor: 0x1e1c1c,
           maxDistance: 34.0,
@@ -24,16 +23,15 @@ export default function Home() {
       if (vantaEffect) vantaEffect.destory();
     };
   }, [vantaEffect]);
-
   return (
     <div className={styles.container}>
-      <Head>
+     <Head>
         <title>Vers3Dynamics</title>
         <link rel="icon" href="favicon.ico" />
       </Head>
       <main className={styles.main} ref={vantaRef}></main>
 <h1><p><script type="module" src="https://unpkg.com/@splinetool/viewer@1.0.79/build/spline-viewer.js"></script>
-<spline-viewer url="https://prod.spline.design/PYL6WDDrqTwo6Lfu/scene.splinecode"></spline-viewer> 𝚑𝚒, 𝚖𝚢 𝚗𝚊𝚖𝚎 𝚒𝚜 <a href="https://my90stv.com/#7XBcT41ImSI">𝓒𝓱𝓻𝓲𝓼𝓽𝓸𝓹𝓱𝓮𝓻</a>.<iframe src="https://vers3dynamicsgame.tiiny.site" width="678" height="500" allowfullscreen></iframe> <html> <head> <title>In the enlightened era of Satya Yuga𓂀369, the universe reveals itself as a multidimensional fractal, a cosmic joy interwoven with the wisdom of error-correcting codes. This infinite fractal echoes itself in a harmonious dance of reflections, each facet imbued with consciousness. Every moment in time holds deep meaning when we explore the intricate patterns within this radiant, meticulously crafted matrix.𓋹</title> </head> <body> <button id="hi again just reminding you that in the space of an inquisitive mind, even the smallest questions can blossom into groundbreaking ideas. Embrace the dance of opposites as your teachers, for they reveal the intricate web of our reality. As you venture deeper, you'll find that local realism falls short, and the quantum world whispers secrets of hermeticism. Double the cube and witness the hidden geometry of gravity, for the apparent duality we perceive is but an illusion. & underneath it all, there is only oneness. Explore, question, and let your mind wander, for it is in the quiet contemplation of these mysteries that we discover our true potential, e pluribus unum|озеро Восток🥼bethesda,md https://www.biblegateway.com/passage/?search=Exodus+37&version=NLT">👋🏾</button> </body> </html> </p>
+<spline-viewer url="https://prod.spline.design/PYL6WDDrqTwo6Lfu/scene.splinecode"></spline-viewer 𝚑𝚒, 𝚖𝚢 𝚗𝚊𝚖𝚎 𝚒𝚜 <a href="https://my90stv.com/#7XBcT41ImSI">𝓒𝓱𝓻𝓲𝓼𝓽𝓸𝓹𝓱𝓮𝓻</a>.<iframe src="https://vers3dynamicsgame.tiiny.site" width="678" height="500" allowfullscreen></iframe> <html> <head> <title>In the enlightened era of Satya Yuga𓂀369, the universe reveals itself as a multidimensional fractal, a cosmic joy interwoven with the wisdom of error-correcting codes. This infinite fractal echoes itself in a harmonious dance of reflections, each facet imbued with consciousness. Every moment in time holds deep meaning when we explore the intricate patterns within this radiant, meticulously crafted matrix.𓋹</title> </head> <body> <button id="hi again just reminding you that in the space of an inquisitive mind, even the smallest questions can blossom into groundbreaking ideas. Embrace the dance of opposites as your teachers, for they reveal the intricate web of our reality. As you venture deeper, you'll find that local realism falls short, and the quantum world whispers secrets of hermeticism. Double the cube and witness the hidden geometry of gravity, for the apparent duality we perceive is but an illusion. & underneath it all, there is only oneness. Explore, question, and let your mind wander, for it is in the quiet contemplation of these mysteries that we discover our true potential, e pluribus unum|озеро Восток🥼bethesda,md https://www.biblegateway.com/passage/?search=Exodus+37&version=NLT">👋🏾</button> </body> </html> </p>
 <p>𝙰Ω 𝚊𝚟𝚒𝚍 𝚠𝚛𝚒𝚝𝚎𝚛, 𝚏𝚘𝚞𝚗𝚍𝚎𝚛 𝚘𝚏 <a href="https://woodyard.eth.limo">versᗱdynamics</a> 𝚊𝚗𝚍 <a href="https://woodyard.dappling.network/">𝓹2𝓹 𝓷𝓮𝓽𝔀𝓸𝓻𝓴 𝓮𝓷𝓰𝓲𝓷𝓮𝓮𝓻</a><a href="https://woodyard.eth.co/">🍎</a></p>
 <p><a href="https://drive.google.com/file/d/1LHW-nsaxzxd-0wkHwYLPR8kkfp1E6iII/view?usp=drivesdk">𝙴𝚡𝚙𝚕𝚘𝚛𝚎</a> 𝚖𝚢 <a href="https://bookstore.dorrancepublishing.com/life-of-a-line/">𝓫𝓸𝓸𝓴</a> "Life of a Line" 𝚝𝚘 𝚎𝚖𝚋𝚊𝚛𝚔 𝚘𝚗 𝚊 <a href="https://drive.google.com/file/d/1onye8f0LnLEvx8olOa-p3xxgbq5jtoAd/view?usp=drivesdk">𝚙𝚘𝚎𝚝𝚒𝚌</a> 𝚊𝚍𝚟𝚎𝚗𝚝𝚞𝚛𝚎 <a href="https://drive.google.com/file/d/14aenR92-dfkjolJBhG3iTCI3Ka6-d6sT/view?usp=drivesdk">📐</a></p>
 <p><a href="https://oncyber.io/stanfordgsb">𝓥𝓲𝓮𝔀 𝓱𝓮𝓻𝓮</a> 𝚝𝚘 <a href="https://drive.google.com/file/d/18lmMGR1HNrSoOntmzaI8ll-EWpUFeNyA/view?usp=drivesdk">𝚜𝚎𝚎</a> 𝚍𝚒𝚐𝚒𝚝𝚊𝚕 𝚏𝚛𝚊𝚐𝚖<a href="https://www.geo.umass.edu/faculty/wclement/Human_rules.html">ᴇɴ</a>𝚝𝚜 𝚍𝚒𝚜𝚙𝚕𝚊𝚢𝚎𝚍 <a href="https://books.google.com/books/about/The_Gospel_of_the_Holy_Twelve.html?id=94YRAQAAIAAJ&printsec=frontcover&source=kp_read_button&hl=en&newbks=1&newbks_redir=0&gboemv=1&ovdme=1#v=onepage&q&f=false">𝚒𝚗</a> 𝚜𝚙𝚊𝚌𝚒𝚊𝚕 𝚌𝚘𝚖𝚙𝚞𝚝𝚒𝚗𝚐 𝚜𝚙𝚊𝚌𝚎𝚜🖼️ </p> <p>𝙳𝚒𝚜𝚌𝚘𝚟𝚎𝚛 𝚖𝚢 <a href="https://chriswoodyard.bandcamp.com/">𝓪𝓾𝓭𝓲𝓽𝓸𝓻𝔂 𝓰𝓮𝓸𝓶𝓮𝓽𝓻𝔂</a> 𝚙𝚛𝚘𝚓𝚎𝚌𝚝🎻<a href="https://drive.google.com/file/d/1PlaDEFBQTRIURd5vC1UPv7QvKUnNluop/view?usp=drivesdk">🎹</a>(𝚊𝚗𝚍 𝚘𝚝𝚑𝚎𝚛 𝚙𝚞𝚋𝚕𝚒𝚜𝚑𝚒𝚗𝚐𝚜📚),</p> <p>𝙴𝚗𝚐𝚊𝚐𝚎 𝚠𝚒𝚝𝚑 𝚊 <a href="https://investng.eth.limo/">𝓰𝓲𝓯</a> 𝚜𝚎𝚊𝚛𝚌𝚑 🎆 </p>
