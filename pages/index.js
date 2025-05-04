13332d3de91d87a5bd1aef7ec3edcf32d92522e2
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2025-05-04 15:49:38 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,6 @@ export default function Home() {
             </p>
           </div>
 
-        <iframe src="https://vers3dynamics.substack.com/embed" width="480" height="320" style="border:1px solid #EEE; background:white;" frameborder="0" scrolling="no"></iframe>
 
           <div className={styles.gifContainer}>
             <iframe
