4b7115ea0c94c213c7777ce53d2e7764902a4e26
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2025-06-08 21:14:32 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -70,7 +70,7 @@ export default function Home() {
             <p className={styles.projectDesc}>
               <a href="https://www.amazon.com/stores/Christopher-Woodyard/author/B084ZSG4V9?ref=ap_rdr&isDramIntegrated=true&shoppingPortalEnabled=true" className={styles.link}>𝓐𝓶𝓪𝔃𝓸𝓷</a>
               <span className={styles.specialText}> — Coloring, poetry and health </span>
-              <a href="https://stellar-resonance-engine.lovable.app/" className={styles.link}>📐</a>
+              <a href="https://humantouch.fun/" className={styles.link}>📐</a>
             </p>
           </div>
 
